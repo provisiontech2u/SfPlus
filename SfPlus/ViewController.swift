ed58166c8982466de56cc0abacdf786653ee4f22
Stephen Tam <stephentam@stephens-mac-mini.local>
2018-03-02 14:35:10 +0800

    Initial Commit

---
+++ SfPlus/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SfPlus
+//
+//  Created by Stephen Tam on 2/3/2018.
+//  Copyright © 2018 Provision Technology. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
